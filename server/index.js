f61608c551104ba44939c656899af30e3695906f
vinaynagasai <142528537+vinaynagasai@users.noreply.github.com>
2026-04-30 19:10:43 +0530

    disable DB for testing

--- server/index.js
+++ server/index.js
@@ -60,7 +60,7 @@ const PORT = process.env.PORT || 5000;
 /* 🔥 Start server */
 const startServer = async () => {
   try {
-    await connectDB();
+    //await connectDB();
     app.listen(PORT, () => {
       console.log(`Server running on port ${PORT}`);
     });
